aeaf28f558ddc003531f76b817c98e538c36893f
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2020-07-10 19:12:04 +0300

    setup.py: Update repository URL

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='rf_client',
-    version='1.0.0',
+    version='1.0.1',
     description='High level RedForester client',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -14,7 +14,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
     ],
-    url='https://github.com/RedForester/rf_client',
+    url='https://github.com/RedForester/python_rf_client',
     author='Red Forester',
     author_email='tech@redforester.com',
     license='MIT',
